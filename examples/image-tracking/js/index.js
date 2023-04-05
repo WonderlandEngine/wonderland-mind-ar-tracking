1bd4b60c1aa6f9c79e5c05c3f9e4370b5200455f
Squareys <squareys@googlemail.com>
2023-04-05 15:42:16 +0200

    Adapt to latest API changes
    Signed-off-by: Squareys <squareys@googlemail.com>

--- examples/image-tracking/js/index.js
+++ examples/image-tracking/js/index.js
@@ -3,20 +3,56 @@
  *
  * This is the entry point of your standalone application.
  *
- * You should register the component you need here, e.g.,
- *
- * ```
- * import { MyComponent } from './my-component.js';
- *
- * WL.registerComponent(MyComponent);
- * ```
- *
- * The `wle:auto-imports:start` and `wle:auto-imports:end` comments are
- * used by the editor as a target for the import list.
+ * There are multiple tags used by the editor to inject code automatically:
+ *     - `wle:auto-imports:start` and `wle:auto-imports:end`: The list of import statements
+ *     - `wle:auto-register:start` and `wle:auto-register:end`: The list of component to register
+ *     - `wle:auto-constants:start` and `wle:auto-constants:end`: The project's constants,
+ *        such as the project's name, whether it should use the physx runtime, etc...
+ *     - `wle:auto-benchmark:start` and `wle:auto-benchmark:end`: Append the benchmarking code
  */
 
+import {loadRuntime} from '@wonderlandengine/api';
+import * as API from '@wonderlandengine/api'; // Deprecated: Backward compatibility.
+
 /* wle:auto-imports:start */
-import '@wonderlandengine/components';
-import { ImageTracking, ImageTrackingTarget } from 'mindar-image-tracking';
-WL.registerComponent(ImageTracking, ImageTrackingTarget);
+import {ImageTracking} from 'mindar-image-tracking';
+import {ImageTrackingTarget} from 'mindar-image-tracking';
 /* wle:auto-imports:end */
+
+/* wle:auto-constants:start */
+const ProjectName = 'mindar-image-tracking';
+const RuntimeBaseName = 'WonderlandRuntime';
+const WithPhysX = false;
+const WithLoader = false;
+/* wle:auto-constants:end */
+
+const engine = await loadRuntime(RuntimeBaseName, {
+    physx: WithPhysX,
+    loader: WithLoader,
+});
+Object.assign(engine, API); // Deprecated: Backward compatibility.
+window.WL = engine; // Deprecated: Backward compatibility.
+
+engine.onSceneLoaded.push(() => {
+    const el = document.getElementById('version');
+    if (el) setTimeout(() => el.remove(), 2000);
+});
+
+const arButton = document.getElementById('ar-button');
+if (arButton) {
+    arButton.dataset.supported = engine.arSupported;
+}
+const vrButton = document.getElementById('vr-button');
+if (vrButton) {
+    vrButton.dataset.supported = engine.vrSupported;
+}
+
+/* wle:auto-register:start */
+engine.registerComponent(ImageTracking);
+engine.registerComponent(ImageTrackingTarget);
+/* wle:auto-register:end */
+
+engine.scene.load(`${ProjectName}.bin`);
+
+/* wle:auto-benchmark:start */
+/* wle:auto-benchmark:end */
