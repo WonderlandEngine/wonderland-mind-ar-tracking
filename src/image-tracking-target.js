297d13d4c799bc639fd5d0a8235387bac4d31928
Squareys <squareys@googlemail.com>
2023-11-07 12:40:02 +0000

    Normalize scaling and scale tracking result instead
    Signed-off-by: Squareys <squareys@googlemail.com>

--- src/image-tracking-target.js
+++ src/image-tracking-target.js
@@ -1,5 +1,10 @@
 import {Component, Type} from '@wonderlandengine/api';
-import {quat2} from 'gl-matrix';
+import {vec3, quat, quat2, mat4} from 'gl-matrix';
+
+const ZERO = [0, 0, 0];
+const tempQuat = quat.create();
+const tempQuat2 = quat2.create();
+const tempVec3 = vec3.create();
 
 /**
  * Make an object follow an image marker
@@ -13,29 +18,44 @@ export class ImageTrackingTarget extends Component {
         arCamera: {type: Type.Object},
     };
 
+    originalScaling = vec3.create();
+
     init() {
         this.arCamera.getComponent('image-tracking').registerTarget(this.targetIndex, this);
+    }
+
+    start() {
+        this.onTrackingLost();
+    }
+
+    onTrackingLost() {
         /* "Hide" object */
-        this.object.scalingLocal.fill(0);
-        this.object.setDirty();
+        this.object.getScalingLocal(this.originalScaling);
+        this.object.setScalingLocal(ZERO);
     }
 
     /* Update tracking target transformation */
-    updateTrack(worldMatrix, markerWidth, markerHeight) {
+    updateTrack(worldMatrix, markerWidth, _) {
         if (!worldMatrix) {
-            /* "Hide" object => invalid tracking */
-            this.object.scalingLocal.fill(0);
-            this.object.setDirty();
+            this.onTrackingLost();
             return;
         }
 
-        quat2.fromMat4(this.object.transformLocal, worldMatrix);
-        quat2.normalize(this.object.transformLocal, this.object.transformLocal);
-        // Anchor point should be the marker center
-        this.object.translateObject([markerWidth / 2, markerHeight / 2, 0]);
-
         const mw = markerWidth / window.devicePixelRatio;
-        this.object.scalingLocal.fill(mw);
-        this.object.setDirty();
+
+        mat4.getRotation(tempQuat, worldMatrix);
+        quat.normalize(tempQuat, tempQuat);
+
+        mat4.getTranslation(tempVec3, worldMatrix);
+        vec3.scale(tempVec3, tempVec3, 1/mw);
+
+        quat2.fromRotationTranslation(tempQuat2, tempQuat, tempVec3);
+
+        this.object.setTransformLocal(tempQuat2);
+        /* Anchor point should be the marker center */
+        const c = window.devicePixelRatio/2;
+        this.object.translateObject([c, c, 0]);
+
+        this.object.setScalingLocal(this.originalScaling);
     }
 }
